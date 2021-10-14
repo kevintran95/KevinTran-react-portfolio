b595fd3123b297e5d4b2a620fb39f1da94301edf
Kevin Tran <kevin_tran95@yahoo.com>
2021-10-13 22:00:44 -0700

    added files and started work on About.js

---
+++ my-app/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section id="about">
+      <div>
+        <h1>My Name is Kevin Trn</h1>
+      </div>
+    </section>
+  )
+}
