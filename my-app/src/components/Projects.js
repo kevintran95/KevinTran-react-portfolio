6775f9d8f19c89758d91d01cc2613f7f50840018
Kevin Tran <kevin_tran95@yahoo.com>
2021-10-16 13:46:06 -0700

    added button functionality

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -6,6 +6,7 @@ import weatherDash from '../assets/weatherdash.png';
 import notetaker from '../assets/notetaker.png';
 import theDegenerate from '../assets/thedegenerate.png';
 
+
 const Projects = () => {
   return (
     <div className= "project-img">
@@ -16,7 +17,7 @@ const Projects = () => {
           <Card.Text>
           Sweet Apples was designed as a web application to allow users to view/post reviews to some of their favorite video games.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" href="https://ancient-plains-02897.herokuapp.com/" target="_blank">Go somewhere</Button>
         </Card.Body>
       </Card>
 
@@ -27,7 +28,7 @@ const Projects = () => {
           <Card.Text>
           A simple weather dashboard, allowing users to choose/ save a city and view weather for the next 5-days. 
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" href="https://kevintran95.github.io/weather-dashboard/" target="_blank">Go somewhere</Button>
         </Card.Body>
       </Card>
 
@@ -38,7 +39,7 @@ const Projects = () => {
           <Card.Text>
           This web application allows a user to choose a sport and view live games as well as lets users place bets. 
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" href="https://syackzan.github.io/The-Degenerate/" target="_blank">Go somewhere</Button>
         </Card.Body>
       </Card>
 
@@ -49,7 +50,7 @@ const Projects = () => {
           <Card.Text>
           Web application that allows a user to create a notes and save them for later use.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary" href="https://glacial-thicket-94833.herokuapp.com" target="_blank">Go somewhere</Button>
         </Card.Body>
       </Card>
 
